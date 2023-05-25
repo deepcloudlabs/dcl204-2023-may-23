60f98c4ab9b543a61f5bf6e1e2101ab09c73ac42
Binnur KURT <deepcloudlabs@gmail.com>
2023-05-25 11:02:07 +0300

    initial commit

--- core-banking/src/com/example/banking/application/BankingApplication.java
+++ core-banking/src/com/example/banking/application/BankingApplication.java
@@ -6,9 +6,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Properties;
 
 import com.example.banking.domain.Account;
+import com.example.banking.domain.CheckingAccount;
+import com.example.banking.domain.Customer;
 
 public class BankingApplication {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -24,6 +27,26 @@ public class BankingApplication {
 		acc1.deposit(0.2);
 		System.out.println("Balance: %8.24f".formatted(acc1.getBalance()));
         System.out.println(acc1.toString()); 
+        
+        Customer jack = new Customer("11111111110", "jack bauer");
+        Account tr1 = new Account("tr1", 100_000);
+		jack.addAccount(tr1);
+        CheckingAccount tr2 = new CheckingAccount("tr2", 200_000,5_000);
+		jack.addAccount(tr2);
+        jack.addAccount(new Account("tr3", 300_000));
+        // Functional Programming: i) Higher-Order Function (HoF) 
+        //                        ii) Pure Function: Lambda Expression
+        jack.getAccount("tr3").ifPresent( account -> account.withdraw(50_000) );
 		
+        Optional<Account> account = jack.getAccount("tr3");
+        if (account.isPresent()) {
+        	Account acc = account.get();
+        	acc.withdraw(50_000);
+        }
+        System.out.println("Number of accounts: %d".formatted(jack.getNumberOfAccounts()));
+        jack.removeAccount(tr1);
+        System.out.println("Number of accounts: %d".formatted(jack.getNumberOfAccounts()));        
+        jack.removeAccount(tr2);
+        System.out.println("Number of accounts: %d".formatted(jack.getNumberOfAccounts()));        
 	} // end of scope -> acc1
 }
--- core-banking/src/com/example/banking/domain/Customer.java
+++ core-banking/src/com/example/banking/domain/Customer.java
@@ -2,9 +2,72 @@ package com.example.banking.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Customer {
-	private String identity;
+	private final String identity;
 	private String fullname;
-	private List<Account> accounts = new ArrayList<>();
+	private final List<Account> accounts = new ArrayList<>();
+
+	public Customer(String identity, String fullname) {
+		this.identity = identity;
+		this.fullname = fullname;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getIdentity() {
+		return identity;
+	}
+
+	public List<Account> getAccounts() {
+		return List.copyOf(accounts);
+	}
+
+	public void addAccount(Account account) {
+		this.accounts.add(account);
+	}
+
+	// overloading vs overriding
+	public Optional<Account> getAccount(int index) {
+		if (index < 0 || index >= accounts.size())
+			return Optional.empty();
+		return Optional.of(this.accounts.get(index));
+	}
+
+	public Optional<Account> getAccount(String iban) {
+		for (Account account : this.accounts) {
+			if (account.getIban().equals(iban))
+				return Optional.of(account);
+		}
+		return Optional.empty();
+	}
+
+	public Optional<Account> getFunctionalAccount(String iban) {
+		// Stream API + FP + Filter/Map/Reduce
+		return accounts.stream().parallel().filter(account -> account.getIban().equals(iban)).findFirst();
+	}
+
+	public int getNumberOfAccounts() {
+		return this.accounts.size();
+	}
+
+	public Optional<Account> removeAccount(String iban) {
+		Optional<Account> account = getAccount(iban);
+		if (account.isEmpty())
+			return Optional.empty();
+		return this.removeAccount(account.get());			
+	}
+
+	public Optional<Account> removeAccount(Account account) {
+		if (this.accounts.remove(account))
+			return Optional.of(account);
+		return Optional.empty();
+	}
 }
